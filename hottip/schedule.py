a709c28ee766c3a29043207c165b47c41f75d4c8
namutaka <namutaka+g@gmail.com>
2019-03-22 02:16:46 +0900

    webとschedulerを分離できるようにした

--- hottip/schedule.py
+++ hottip/schedule.py
@@ -1,8 +1,6 @@
-from datetime import datetime
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from django.conf import settings
 import logging
 from .models import Distributor
 
@@ -10,29 +8,29 @@ logger = logging.getLogger(__name__)
 
 scheduler = BackgroundScheduler()
 
-
 def start():
+    scheduler.configure(timezone=settings.TIME_ZONE)
+
+    scheduler.add_job(_reload_jobs, 'interval', minutes=1, id='reload_jobs')
+    _reload_jobs()
+
     logger.info('Start hottip scheduler')
-    scheduler.add_job(_monitor, 'interval', minutes=1)
     scheduler.start()
 
-    for d in Distributor.objects.all():
-        add_distoributor_emit_job(d)
 
-    scheduler.print_jobs()
-
-
-def add_distoributor_emit_job(distributor):
-    logger.info('add distoribution job: %s', vars(distributor))
-    scheduler.add_job(
-        _emit_distributor,
-        CronTrigger(**distributor.schedule),
-        args=[distributor.id],
-        id=f'distribute:{distributor.id}',
-        replace_existing=True)
+def setup_distributor_jobs():
+    for distributor in Distributor.objects.all():
+        logger.info('add distoribution job: %s', distributor)
+        scheduler.add_job(
+            _emit_distributor,
+            CronTrigger(**distributor.schedule),
+            args=[distributor.id],
+            id=f'distribute:{distributor.id}',
+            replace_existing=True)
 
 
-def _monitor():
+def _reload_jobs():
+    setup_distributor_jobs()
     scheduler.print_jobs()
 
 
@@ -40,9 +38,4 @@ def _emit_distributor(distributor_id):
     distributor = Distributor.objects.get(id=distributor_id)
     logger.info(f'emitted distributor: {distributor_id}')
     distributor.distribute()
-
-
-@receiver(post_save, sender=Distributor)
-def distributor_handler(sender, instance, **kwargs):
-    add_distoributor_emit_job(instance)
 
