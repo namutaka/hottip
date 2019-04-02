0d305711671afd05fd9c4fc091570f12e7240aff
namutaka <namutaka+g@gmail.com>
2019-04-02 22:45:57 +0900

    vue add apollo vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,13 @@
 import Vue from "vue";
+import './plugins/vuetify'
 import App from "./App.vue";
 import router from "./router";
+import { createProvider } from "./vue-apollo";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount("#app");
