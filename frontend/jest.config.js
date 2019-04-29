91dab4a336c2e5c8ed886c1070ec1a75a1ae6514
namutaka <namutaka+g@gmail.com>
2019-04-29 19:55:38 +0900

    vssでのデバッグ用設定

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   snapshotSerializers: ["jest-serializer-vue"],
   testMatch: [
-    "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+    "<rootDir>/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
   ],
   testURL: "http://localhost/",
   globals: {
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,5 +10,8 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  configureWebpack: {
+    devtool: 'source-map'
   }
 }
