c685d47634b66253955efbb7389338a76bd90091
namutaka <namutaka+g@gmail.com>
2019-05-05 23:49:04 +0900

    データの保持をPageが担当するように

--- frontend/src/components/ChannelList.vue
+++ frontend/src/components/ChannelList.vue
@@ -4,22 +4,22 @@
 
     <v-data-table
       :headers="headers"
-      :items="allChannels.edges"
+      :items="channels"
       :loading="loading"
       no-data-text="no-data"
       class="elevation-1"
       hide-actions
     >
       <template v-slot:items="props">
-        <tr span @click="clickChannel(props.item.node)">
-          <td>{{ props.item.node.rawId }}</td>
-          <td class="text-sm-left text-wrap">{{ props.item.node.name }}</td>
+        <tr span @click="clickChannel(props.item)">
+          <td>{{ props.item.rawId }}</td>
+          <td class="text-sm-left text-wrap">{{ props.item.name }}</td>
           <td class="text-sm-left text-wrap">
-            {{ props.item.node.description }}
+            {{ props.item.description }}
           </td>
           <td class="font-weight-light">
             <v-icon small class="mx-1">update</v-icon>
-            {{ props.item.node.updatedAt | datetime }}
+            {{ props.item.updatedAt | datetime }}
           </td>
         </tr>
       </template>
@@ -29,38 +29,23 @@
 
 <script lang="ts">
 import { Component, Prop, Emit, Vue } from 'vue-property-decorator';
-import { ALL_CHANNELS } from '@/graphql/queries';
 import { Channel } from '@/types/models';
 
-@Component({
-  apollo: {
-    allChannels: {
-      query: ALL_CHANNELS,
-    },
-  },
-})
+@Component({})
 export default class ChannelList extends Vue {
   $refs!: {
     form: HTMLFormElement;
   };
 
-  allChannels: Object = {};
-
-  mounted() {
-    // ページ遷移時に内容を確実に更新
-    this.$apollo.queries.allChannels.refetch();
-  }
+  @Prop() channels!: Channel[];
+  @Prop() loading!: boolean;
 
   readonly headers = [
-    { text: 'ID', value: 'id', width: '30' },
+    { text: 'ID', value: 'rawId', width: '30' },
     { text: '名前', value: 'name' },
     { text: '説明', value: 'description' },
     { text: '日付', value: 'createdAt', width: '180' },
   ];
-
-  get loading(): boolean {
-    return this.$apollo.queries.allChannels.loading;
-  }
 
   @Emit()
   clickChannel(channel: Channel) {
--- frontend/src/views/Channels.vue
+++ frontend/src/views/Channels.vue
@@ -2,7 +2,11 @@
   <div class="channel_list">
     <ChannelForm ref="channelForm" @change-channel="openChannel" />
 
-    <ChannelList @click-channel="openChannel">
+    <ChannelList
+      :channels="channels"
+      @click-channel="openChannel"
+      :loading="loading"
+      >
       <v-btn color="primary"
         dark
         class="mb-2"
@@ -17,16 +21,42 @@ import { Component, Vue } from 'vue-property-decorator';
 import ChannelList from '@/components/ChannelList.vue';
 import ChannelForm from '@/components/ChannelForm.vue';
 import { Channel } from '@/types/models';
+import { ALL_CHANNELS } from '@/graphql/queries';
+import { allChannels_allChannels } from '@/graphql/types/allChannels'
 
 @Component({
   components: {
     ChannelList,
     ChannelForm,
   },
+  apollo: {
+    allChannels: {
+      query: ALL_CHANNELS,
+    },
+  },
 })
 export default class Channels extends Vue {
   $refs!: {
     channelForm: ChannelForm
+  }
+
+  allChannels!: allChannels_allChannels | null;
+
+  mounted() {
+    // ページ遷移時に内容を確実に更新
+    this.$apollo.queries.allChannels.refetch();
+  }
+
+  get loading(): boolean {
+    return this.$apollo.queries.allChannels.loading;
+  }
+
+  get channels() {
+    if (this.allChannels && this.allChannels.edges) {
+      return this.allChannels.edges.map((edge: any) => edge.node);
+    } else {
+      return [];
+    }
   }
 
   add() {
